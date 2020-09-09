7a602b5af259a2d0cd9a932439d2ee51ccf8f1ff
RemKuzo <rikuzo36@gmail.com>
2020-09-09 22:05:34 +0200

    ft: adding user geolocation
    [in progress

---
+++ ui/scripts/location.js
@@ -0,0 +1,21 @@
+var getLocation = document.getElementById('getlocation');
+
+getLocation.addEventListener('click', evt=> {
+  if('geolocation' in navigator) {
+      navigator.geolocation.getCurrentPosition(position=>{
+        let latitude = position.coords.latitude;
+        let longitude = position.coords.longitude;
+
+        let googleMapURL = `https://maps.googleapis.com/maps/api/staticmap?center=${latitude}, ${longitude}&zoom=11&size=400x300`;
+        const mapImage = document.getElementById('mapImage');
+        mapImage.src = googleMapURL;
+
+        console.log(latitude, longitude);
+      }, error=>{
+          console.log(error.code)
+      });
+  } 
+  else {
+      console.log('Not Supported');
+  }
+});
