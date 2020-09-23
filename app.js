3d7cfe65e430d4c2cd43deb43be9d5193493dada
RemKuzo <rikuzo36@gmail.com>
2020-09-23 16:06:55 +0200

    ft: route for displaying all users added a babel file added a .env file added a .gitignore file initialized npm packages added mogodb configuration added user controllers added user model added a users routes
    [done]

---
+++ app.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import userRouters from "./server/routers/userRouters";
+
+ require ('./server/config/mongoose')
+const app = express();
+
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+
+app.use(userRouters)
+
+
+
+export default app
