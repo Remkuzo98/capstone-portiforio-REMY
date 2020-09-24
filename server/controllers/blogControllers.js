0c4d6b08ea2b58621f262a160fe359a828a68d17
RemKuzo <rikuzo36@gmail.com>
2020-09-24 16:03:33 +0200

    ft: delete a blog added a blog file controller added a blog route [done]

--- server/controllers/blogControllers.js
+++ server/controllers/blogControllers.js
@@ -1,29 +1,24 @@
 import Blog from '../models/blog'
 export default new class blogControllers {
-    async getbyId(req, res){
+    async delete(req, res){
         try {
-            const blog = await Blog.findOne({_id:req.params.id});
-            if(blog){
-                return res.status(200).send({
-                    message: 'Blog Found',
-                    data: {
-                        blog
-                    }})
-            }
-            else
-            {
+            const blog = Blog.findOne({_id:req.params.id});
+            if(!blog){
                 return res.status(400).send({
-                    message: 'Blog not Found'
+                    message:"Blog not Found"
                 })
             }
+            await blog.remove();
+            return res.status(400).send({
+                message:"Blog removed "
+            })
+
         } catch (error) {
             return res.status(500).send({
                 error:error.message
             })
         }
     }
-    
-
     
 }
 
