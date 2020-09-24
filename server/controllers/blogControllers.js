99b27d0306fdc5914a34510418bbba0c86d42ab8
RemKuzo <rikuzo36@gmail.com>
2020-09-24 17:02:10 +0200

    ft: update a blog
    added a controller file
    added a router file
    
    [done]

--- server/controllers/blogControllers.js
+++ server/controllers/blogControllers.js
@@ -1,21 +1,31 @@
 import Blog from '../models/blog'
 export default new class blogControllers {
-    async delete(req, res){
-        try {
-            const blog = Blog.findOne({_id:req.params.id});
-            if(!blog){
-                return res.status(400).send({
-                    message:"Blog not Found"
-                })
-            }
-            await blog.remove();
-            return res.status(400).send({
-                message:"Blog removed "
+    async update(req, res){
+        const blog = await Blog.findById({_id:req.params.id})
+        const AllowedUpdates = ['tittle','date','body']
+        const updates = Object.keys(req.body)
+        const isValidOperation = updates.every((update) => AllowedUpdates.includes(update))
+        if(!isValidOperation){
+            return res.status(404).send({
+                message: 'Invalid Data Fields Present'
             })
-
+        }
+        try {
+            updates.forEach((update) => {
+                blog[update] = req.body[update]
+            })
+            await blog.save()
+            if(!blog){
+                return res.status(404).send({message:'An error occured'})
+            }
+            return res.status(200).send({
+                message: 'Blog modified',
+                data: {
+                    blog
+                }})
         } catch (error) {
-            return res.status(500).send({
-                error:error.message
+            return res.status(400).send({
+                message: error.message
             })
         }
     }
