ca88e39ea8bac399cbce0a08afbce67bc4fe28bd
RemKuzo <rikuzo36@gmail.com>
2020-09-24 15:33:03 +0200

    ft: get a single blog
    added a blog controller file
    added a blog router file
    
    [done]

--- server/controllers/blogControllers.js
+++ server/controllers/blogControllers.js
@@ -1,67 +1,30 @@
 import Blog from '../models/blog'
 export default new class blogControllers {
-    async getAll(req, res) {
+    async getbyId(req, res){
         try {
-            const blog = await Blog.find({});
+            const blog = await Blog.findOne({_id:req.params.id});
             if(blog){
                 return res.status(200).send({
-                    message: "new Data is Here",
-                    data : {
-                        blog:blog
-                    }
-                })
+                    message: 'Blog Found',
+                    data: {
+                        blog
+                    }})
             }
             else
             {
                 return res.status(400).send({
-                    message: "Data not Found"
+                    message: 'Blog not Found'
                 })
             }
-            
         } catch (error) {
             return res.status(500).send({
-                error: error.message
+                error:error.message
             })
         }
-        
     }
-      
     
 
     
 }
 
 
-// export default new class blogControllers {
-
-//     async update(req, res){
-//         const user = await User.findById({_id:req.params.id})
-//         const AllowedUpdates = ['fullname','username','email']
-//         const updates = Object.keys(req.body)
-//         const isValidOperation = updates.every((update) => AllowedUpdates.includes(update))
-//         if(!isValidOperation){
-//             return res.status(404).send({
-//                 message: 'Invalid Data Fields Present'
-//             })
-//         }
-//         try {
-//             updates.forEach((update) => {
-//                 user[update] = req.body[update]
-//             })
-//             await user.save()
-//             if(!user){
-//                 return res.status(404).send({message:'An error occured'})
-//             }
-//             return res.status(200).send({
-//                 message: 'user modified',
-//                 data: {
-//                     user
-//                 }})
-//         } catch (error) {
-//             return res.status(400).send({
-//                 message: error.message
-//             })
-//         }
-//     }
-   
-// }
