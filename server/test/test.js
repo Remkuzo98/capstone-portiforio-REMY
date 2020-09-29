c161197c83d36a758652d6ec7542b3fc8fb427c2
RemKuzo <rikuzo36@gmail.com>
2020-09-30 00:03:10 +0200

    ft: getting a single blog
    modified an index file
    added a test file
    added a test script path
    install packages
    
    [in progress]

---
+++ server/test/test.js
@@ -0,0 +1,24 @@
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import index from '../../index';
+ chai.should();
+ chai.use(chaiHttp);
+
+ describe('users', () => {
+   
+    describe('get a blog', () => {
+       
+        it('should get a single blog',(done) => {
+            chai.request(index)
+            .get('/api/blogs/:id')
+            .end((err, res) => {
+             res.should.have.status(200);
+             done();
+            })
+         
+        })
+
+    })
+
+
+ })
