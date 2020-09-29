1243cadf06e85e80a01ff294e47b7836757b641e
RemKuzo <rikuzo36@gmail.com>
2020-09-29 21:46:31 +0200

    ft: saving a user modified index file modified package-lock.json added test file [in progress]

---
+++ server/test/test.js
@@ -0,0 +1,26 @@
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import index from '../../index';
+ chai.should();
+ chai.use(chaiHttp);
+
+ describe('users', () => {
+   
+    describe('save a user', () => {
+       
+        it('should save a user',(done) => {
+            chai.request(index)
+            .post('/api/users')
+            .end((err, res) => {
+             res.should.have.status(200);
+             res.should.have.lengthOf(3);
+             done();
+            })
+         
+        })
+
+    })
+
+
+ })
+
