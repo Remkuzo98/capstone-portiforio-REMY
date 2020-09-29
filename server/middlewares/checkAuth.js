f2f401c0992ce742871fe39bf72b47c9bbccb16c
RemKuzo <rikuzo36@gmail.com>
2020-09-29 17:04:55 +0200

    delete a blog
    adding authentication middleware

---
+++ server/middlewares/checkAuth.js
@@ -0,0 +1,23 @@
+import jwt from 'jsonwebtoken';
+
+
+
+const autho = (req, res, next) => {
+   try {
+      const  token  = req.headers.authorization;
+      const decoded = jwt.verify(token, process.env.jwtKey)
+      if(decoded){
+          next();
+      }else {
+          return res.status(401).send({
+            message:"authethication failed",
+
+            })
+      }
+   } catch (error) {
+    return res.status(500).send({
+        error:error.message
+    })
+   }
+}
+export default autho;
--- server/routers/blogRouters.js
+++ server/routers/blogRouters.js
@@ -1,11 +1,11 @@
 import express from 'express';
 import blogControllers from "../controllers/blogControllers";
-
+import checkAuth from "../middlewares/checkAuth"
 const blogRouters = express.Router();
 
 
 
-blogRouters.delete('/api/blogs/:id',blogControllers.delete)
+blogRouters.delete('/api/blogs/:id',checkAuth,blogControllers.delete)
 
 
 export default blogRouters
