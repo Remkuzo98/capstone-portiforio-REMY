482b7c81a407319d32c44328cfde8759b57710c3
Remkuzo98 <53525195+Remkuzo98@users.noreply.github.com>
2020-09-29 23:25:02 +0200

    Update blogControllers.js

--- server/controllers/blogControllers.js
+++ server/controllers/blogControllers.js
@@ -30,38 +30,3 @@ export default new class blogControllers {
 
     
 }
-
-
-// export default new class blogControllers {
-
-//     async update(req, res){
-//         const user = await User.findById({_id:req.params.id})
-//         const AllowedUpdates = ['fullname','username','email']
-//         const updates = Object.keys(req.body)
-//         const isValidOperation = updates.every((update) => AllowedUpdates.includes(update))
-//         if(!isValidOperation){
-//             return res.status(404).send({
-//                 message: 'Invalid Data Fields Present'
-//             })
-//         }
-//         try {
-//             updates.forEach((update) => {
-//                 user[update] = req.body[update]
-//             })
-//             await user.save()
-//             if(!user){
-//                 return res.status(404).send({message:'An error occured'})
-//             }
-//             return res.status(200).send({
-//                 message: 'user modified',
-//                 data: {
-//                     user
-//                 }})
-//         } catch (error) {
-//             return res.status(400).send({
-//                 message: error.message
-//             })
-//         }
-//     }
-   
-// }
